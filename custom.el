b422817bda0819920e36fc6578cb807be8400fbf
James Tranovich <jtranovich@gmail.com>
2013-01-06 11:44:47 -0800

    commented out currently broken code.

--- custom.el
+++ custom.el
@@ -9,5 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Liberation Mono" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(default ((t (:family "Liberation Mono" :foundry "unknown" :slant normal :weight normal :height 122 :width normal))))
  '(flyspell-duplicate ((t (:inherit warning :foreground "pale turquoise" :weight bold)))))
