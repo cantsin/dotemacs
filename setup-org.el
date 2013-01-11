d0f92ed232d05a000ac80e7acbb37244e7ffa8c7
James Tranovich <jtranovich@gmail.com>
2013-01-11 06:14:38 -0800

    Org-mode improvements.
    Don't let org-mode keybindings conflict with windmove.
    Default key bindings for useful org-mode functions.

--- setup-org.el
+++ setup-org.el
@@ -10,4 +10,9 @@
 (setq org-startup-indented t)
 (setq org-archive-location ".archived.org::* From %s")
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (provide 'setup-org)
--- setup-windmove.el
+++ setup-windmove.el
@@ -3,4 +3,10 @@
 (global-set-key (kbd "C-c j")  'windmove-left)
 (global-set-key (kbd "C-c k") 'windmove-right)
 
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (provide 'setup-windmove)
