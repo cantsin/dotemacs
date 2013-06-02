1a20a856f07de673ece073364dbd894c900078ac
James Tranovich <jtranovich@gmail.com>
2013-06-02 14:55:47 -0700

    Added flycheck, and cleaned up some code.

--- setup-erc.el
+++ setup-erc.el
@@ -1,3 +1,7 @@
+;;; setup-erc -- Summary
+;;; Commentary:
+;;; Setup all things ERC-related.
+;;; Code:
 (require 'erc)
 (require 'erc-match)
 (require 'erc-join)
@@ -28,23 +32,23 @@
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
 (defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
+  "Save the ERC logs."
   (save-some-buffers t (lambda () (when (and (eq major-mode 'erc-mode)
                                              (not (null buffer-file-name)))))))
 (add-hook 'erc-insert-post-hook 'erc-save-buffer-in-logs)
 
 ;;; Notify me when a keyword is matched (someone wants to reach me)
 (defvar my-erc-page-message "%s is calling your name."
-  "Format of message to display in dialog box")
+  "Format of message to display in dialog box.")
 
 (defvar my-erc-page-nick-alist nil
-  "Alist of nicks and the last time they tried to trigger a
-notification")
+  "Alist of nicks and the last time they tried to trigger a notification.")
 
 (defvar my-erc-page-timeout 30
-  "Number of seconds that must elapse between notifications from
-the same person.")
+  "Number of seconds that must elapse between notifications from the same person.")
 
 (defun my-erc-page-popup-notification (nick)
+  "Notify using the WM.  NICK should be part of the message."
   (when window-system
     ;; must set default directory, otherwise start-process is unhappy
     ;; when this is something remote or nonexistent
@@ -72,8 +76,9 @@ that can occur between two notifications.  The default is
       t)))
 
 (defun my-erc-page-me (match-type nick message)
-  "Notify the current user when someone sends a message that
-matches a regexp in `erc-keywords'."
+  "Notify the user when someone sends a message that \\
+will match a regexp in `erc-keywords'.  Parameters: MATCH-TYPE, \\
+NICK, MESSAGE."
   (interactive)
   (when (and (eq match-type 'keyword)
              ;; I don't want to see anything from the erc server
@@ -86,6 +91,7 @@ matches a regexp in `erc-keywords'."
 (add-hook 'erc-text-matched-hook 'my-erc-page-me)
 
 (defun my-erc-page-me-PRIVMSG (proc parsed)
+  "Page the user if a PRIVMSG arrives.  PROC is not used.  PARSED is the message."
   (let ((nick (car (erc-parse-user (erc-response.sender parsed))))
         (target (car (erc-response.command-args parsed)))
         (msg (erc-response.contents parsed)))
@@ -120,3 +126,4 @@ matches a regexp in `erc-keywords'."
   (znc-all))
 
 (provide 'setup-erc)
+;;; setup-erc.el ends here
