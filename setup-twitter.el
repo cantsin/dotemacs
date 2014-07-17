fd9314eaaf7885c46a5658588df4aa4794078246
James Tranovich <jtranovich@gmail.com>
2014-07-17 13:28:36 -0700

    Better twittering-mode key bindings.

--- setup-twitter.el
+++ setup-twitter.el
@@ -9,6 +9,14 @@
 (setq twittering-use-master-password t)
 (setq twittering-url-show-status nil)
 
+(setq twittering-tinyurl-service 'bit.ly)
+
+;; swap p,n with j,k
+(define-key twittering-mode-map (kbd "n") 'twittering-goto-next-status)
+(define-key twittering-mode-map (kbd "p") 'twittering-goto-previous-status)
+(define-key twittering-mode-map (kbd "j") 'twittering-goto-next-status-of-user)
+(define-key twittering-mode-map (kbd "k") 'twittering-goto-previous-status-of-user)
+
 (add-hook 'twittering-mode
           '(lambda () (visual-line-mode)))
 
