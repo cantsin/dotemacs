20272b09bea38313a501af97f80fcf809be3404f
James Tranovich <jtranovich@gmail.com>
2014-11-06 08:39:23 -0800

    Use ispell/abbrev to autocorrect mistakes.

--- setup-misc.el
+++ setup-misc.el
@@ -297,5 +297,25 @@ prefix argument."
 
 (global-set-key (kbd "M-g M-c") 'go-to-column)
 
+;; use abbrev to memorize mistakes
+(global-set-key (kbd "C-x C-i") 'endless/ispell-word-then-abbrev)
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word'.  Then create an abbrev for the correction made.
+With prefix P, create local abbrev.  Otherwise it will be global."
+  (interactive "P")
+  (let ((bef (downcase (or (thing-at-point 'word) ""))) aft)
+    (call-interactively 'ispell-word)
+    (setq aft (downcase (or (thing-at-point 'word) "")))
+    (unless (string= aft bef)
+      (message "\"%s\" now expands to \"%s\" %sally"
+               bef aft (if p "loc" "glob"))
+      (define-abbrev
+        (if p local-abbrev-table global-abbrev-table)
+        bef aft))))
+
+(setq save-abbrevs t)
+(setq-default abbrev-mode t)
+
 (provide 'setup-misc)
 ;;; setup-misc.el ends here
