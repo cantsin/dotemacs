4c119e42546a72255a4689c70870f85bf1090c46
James Tranovich <jtranovich@gmail.com>
2015-04-01 14:07:58 -0700

    use-package org.

--- setup-org.el
+++ setup-org.el
@@ -2,27 +2,7 @@
 ;;; Commentary:
 ;;; Setup org-mode.
 ;;; Code:
-(require 'org)
-(require 'org-agenda)
-(require 'org-bullets)
-(require 'org-capture)
-
-(setq org-use-speed-commands t)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (sh . t)))
-
-(setq org-confirm-babel-evaluate nil)
-
-(setq org-src-fontify-natively t)
-(setq org-src-tab-acts-natively t)
-
-(add-hook 'org-mode-hook
-          (lambda () (org-bullets-mode 1)))
-(add-hook 'org-after-todo-state-change-hook
-          (lambda () (org-update-statistics-cookies t)))
+(require 'use-package)
 
 (defun update-parent-cookie ()
   "Update the count of items in this section."
@@ -40,53 +20,63 @@
   "When killing the whole line, also update the cookie."
   (update-parent-cookie))
 
-;; (defadvice org-archive-subtree (after fix-cookies activate)
-;;   (update-parent-cookie))
+(defun cantsin/org-init ()
+  "Initialize org."
+  (use-package org-bullets
+    :ensure t)
+  (use-package stripe-buffer
+    :ensure t)
+  (add-hook 'org-mode-hook
+            (lambda () (org-bullets-mode 1)))
+  (add-hook 'org-after-todo-state-change-hook
+            (lambda () (org-update-statistics-cookies t)))
+  (add-hook 'org-mode-hook 'org-table-stripes-enable)
+  (add-hook 'org-mode-hook
+            #'(lambda () (setq electric-indent-mode nil))))
 
-(require 'stripe-buffer)
+(defun cantsin/org-config ()
+  "Set up org."
+  (setq org-use-speed-commands t
+        org-clock-persist 'history
+        org-confirm-babel-evaluate nil
+        org-src-fontify-natively t
+        org-src-tab-acts-natively t
+        org-startup-indented t
+        org-return-follows-link t
+        org-enforce-todo-dependencies t
+        org-enforce-todo-checkbox-dependencies t
+        org-use-fast-todo-selection t
+        org-archive-location ".archived.org::* From %s")
+  (setq org-stuck-projects
+        '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file+headline org-default-notes-file "Tasks")
+           "* TODO %?\n  %i\n  %a")
+          ("r" "respond" entry (file org-default-notes-file)
+           "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n\n")))
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "PENDING(p)" "|" "DONE(d)")
+          (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+          (sequence "|" "FUTURE(l)")
+          (sequence "|" "CANCELED(c)")))
+  ;; set up shortcut for priorities
+  (setq org-agenda-custom-commands
+        '(("p" "Agenda for all priorities" agenda ""
+           ((org-agenda-skip-function
+             '(and
+               (not
+                (org-entry-get nil "PRIORITY"))
+               (point-at-eol)))))))
+  (org-clock-persistence-insinuate))
 
-(add-hook 'org-mode-hook 'org-table-stripes-enable)
-
-(setq org-startup-indented t)
-(setq org-return-follows-link t)
-(setq org-enforce-todo-dependencies t)
-(setq org-enforce-todo-checkbox-dependencies t)
-(setq org-use-fast-todo-selection t)
-(setq org-archive-location ".archived.org::* From %s")
-(setq org-stuck-projects
-      '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline org-default-notes-file "Tasks")
-         "* TODO %?\n  %i\n  %a")
-        ("r" "respond" entry (file org-default-notes-file)
-         "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n\n")))
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "PENDING(p)" "|" "DONE(d)")
-        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
-        (sequence "|" "FUTURE(l)")
-        (sequence "|" "CANCELED(c)")))
-
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(global-set-key "\C-ce" 'org-archive-subtree)
-
-;; set up shortcut for priorities
-(setq org-agenda-custom-commands
-      '(("p" "Agenda for all priorities" agenda ""
-          ((org-agenda-skip-function
-            '(and
-              (not
-               (org-entry-get nil "PRIORITY"))
-              (point-at-eol)))))))
-
-(add-hook 'org-mode-hook
-          #'(lambda () (setq electric-indent-mode nil)))
-
-(require 'org-clock)
-(setq org-clock-persist 'history)
-(org-clock-persistence-insinuate)
+(use-package org
+  :bind (("C-c l" . org-store-link)
+         ("C-c c" . org-capture)
+         ("C-c a" . org-agenda)
+         ("C-c b" . org-iswitchb)
+         ("C-c e" . org-archive-subtree))
+  :init (cantsin/org-init)
+  :config (cantsin/org-config))
 
 (provide 'setup-org)
 ;;; setup-org.el ends here
