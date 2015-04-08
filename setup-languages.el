55f0860213290f6ec03e981bea0feb0500558cc3
James Tranovich <james@tranovi.ch>
2015-04-07 17:28:19 -0700

    use-package various language configurations.

--- setup-languages.el
+++ setup-languages.el
@@ -4,43 +4,38 @@
 ;;; Code:
 (require 'use-package)
 
-;; indentation.
-(require 'js)
+(use-package lua-mode
+  :defer t
+  :config (setq lua-indent-level 2))
+
+;; auto disassemble llvm when opening .bc files
+(use-package autodisass-llvm-bitcode
+  :defer t)
+
+(use-package alchemist
+  :defer t
+  :config (setq alchemist-project-compile-when-needed t))
+
+(defun cantsin/setup-jedi ()
+  "Configure jedi (python)."
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:complete-on-dot t))
+
+(use-package jedi
+  :defer t
+  :config (cantsin/setup-jedi))
+
+;; Github README.mds.
+(use-package gfm-mode
+  :defer t
+  :config (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode)))
+
 (setq js-indent-level 2)
 (setq c-basic-offset 4)
 (c-set-offset 'substatement-open 0)
 
-(require 'lua-mode)
-(setq lua-indent-level 2)
-
-;; auto disassemble llvm when opening .bc files
-(require 'autodisass-llvm-bitcode)
-
-;; Github README.mds.
-(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
-
-;; skewer-mode.
-(add-hook 'js2-mode-hook 'skewer-mode)
-(add-hook 'css-mode-hook 'skewer-css-mode)
-(add-hook 'html-mode-hook 'skewer-html-mode)
-
-;; tagedit
-(eval-after-load "sgml-mode"
-  '(progn
-     (require 'tagedit)
-     (tagedit-add-paredit-like-keybindings)
-     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
-
 ;; (load-file (let ((coding-system-for-read 'utf-8))
 ;;                 (shell-command-to-string "agda-mode locate")))
-
-(require 'jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-
-;; alchemist
-(require 'alchemist)
-(setq alchemist-project-compile-when-needed t)
 
 (provide 'setup-languages)
 ;;; setup-languages.el ends here
