a1778f6547158fd38b23986dc6c1452b17e814a3
James Tranovich <james@tranovi.ch>
2015-04-07 17:11:40 -0700

    use-package haskell.

---
+++ setup-haskell.el
@@ -0,0 +1,30 @@
+;;; setup-haskell -- Summary
+;;; Commentary:
+;;; Setup for Haskell and related.
+;;; Code:
+(require 'use-package)
+
+(use-package purscheck
+  :defer t
+  :init (add-to-list 'load-path "purscheck.el")) ;; custom
+
+(defun cantsin/setup-purescript ()
+  "Set up purescript."
+  (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
+  (load "purescript-mode-autoloads"))
+
+(use-package purescript-mode
+  :defer t
+  :ensure t
+  :config (cantsin/setup-purescript))
+
+(defun cantsin/setup-haskell ()
+  "Set up haskell."
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent))
+
+(use-package haskell-mode
+  :defer t
+  :init (cantsin/setup-haskell))
+
+;;; setup-haskell.el ends here
--- setup-languages.el
+++ setup-languages.el
@@ -4,102 +4,6 @@
 ;;; Code:
 (require 'use-package)
 
-;; clojure
-(require 'cider)
-(setq cider-show-error-buffer nil)
-
-(add-to-list 'same-window-buffer-names "*cider*")
-
-;; default hooks.
-(add-hook 'clojure-mode-hook 'subword-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-mode-hook 'subword-mode)
-(add-hook 'cider-mode-hook 'paredit-mode)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-;;(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-repl-mode-hook 'subword-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-;;(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-(add-hook 'lisp-mode-hook 'paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'lisp-interaction-mode-hook 'paredit-mode)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-
-;; pretty-print evals.
-(global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
-(global-set-key [remap eval-expression] 'pp-eval-expression)
-
-;; paredit.
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-
-;; make paredit and eldoc play nice.
-(eldoc-add-command 'paredit-backward-delete 'paredit-close-round)
-
-;; paren-mode.
-(require 'paren)
-(show-paren-mode 1)
-(setq show-paren-delay 0)
-(setq show-paren-style 'mixed)
-
-;; elisp
-(defun eval-and-replace ()
-  "Replace the preceding sexp with its value."
-  (interactive)
-  (backward-kill-sexp)
-  (condition-case nil
-      (prin1 (eval (read (current-kill 0)))
-             (current-buffer))
-    (error (message "Invalid expression")
-           (insert (current-kill 0)))))
-(global-set-key (kbd "C-c v") 'eval-buffer)
-;; Should be able to eval-and-replace anywhere.
-(global-set-key (kbd "C-c C-e") 'eval-and-replace)
-
-(require 'smart-compile)
-
-;; work in progress. requires some setup. no error checking!
-(defun recompile-if-extant ()
-  "Recompile, assumes the *compilation* frame exists."
-  (interactive)
-  (switch-to-buffer-other-frame "*compilation*")
-  (recompile))
-
-(global-set-key (kbd "C-c C-c") 'recompile-if-extant)
-
-(electric-indent-mode +1)
-(add-hook 'python-mode-hook
-          #'(lambda () (setq electric-indent-mode nil)))
-
-;; always use the same window.
-(setq compilation-buffer-name-function
-      '(lambda (mode)
-     "*compilation*"))
-
-(setq compilation-scroll-output 'first-error)
-
-;; use M-. and M-, to jump to definitions
-(autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-(add-hook 'emacs-lisp-mode-hook
-          (lambda () (elisp-slime-nav-mode t)))
-(eval-after-load 'elisp-slime-nav
-  '(diminish 'elisp-slime-nav-mode))
-
-;; haskell
-(add-to-list 'smart-compile-alist
-             '("\\.hs\\'" . "ghc -o %n --make %f"))
-(add-to-list 'smart-compile-alist
-             '("\\.rs\\'" . "rust build %f"))
-
-;; racer -- only comment out if installed
-;; (setq racer-rust-src-path "~/rust/src/")
-;; (setq racer-cmd "~/racer/target/release/racer")
-;; (add-to-list 'load-path "~/racer/editors")
-;; (eval-after-load "rust-mode" '(require 'racer))
-
 ;; indentation.
 (require 'js)
 (setq js-indent-level 2)
@@ -108,9 +12,6 @@
 
 (require 'lua-mode)
 (setq lua-indent-level 2)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
 ;; auto disassemble llvm when opening .bc files
 (require 'autodisass-llvm-bitcode)
@@ -162,16 +63,6 @@
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
-
-;; purscheck
-(add-to-list 'load-path "purscheck.el") ;; custom
-(require 'purscheck)
-
-;; enable purescript smart indentation and purscheck whenever a
-;; purescript file is loaded into emacs
-(require 'purescript-mode)
-(add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
-(load "purescript-mode-autoloads")
 
 ;; alchemist
 (require 'alchemist)
