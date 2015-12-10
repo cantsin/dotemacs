b8d6d7c986df5c28b9fba38b480160f374d9b68d
James Tranovich <jtranovich@gmail.com>
2015-12-10 07:44:28 -0800

    javascript tweak.

--- setup-languages.el
+++ setup-languages.el
@@ -36,6 +36,7 @@
   :ensure t
   :init (progn
           (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+          (setq-default js2-basic-offset 2)
           (setq js-indent-level 2)
           (add-hook 'js-mode-hook 'js2-minor-mode)))
 
