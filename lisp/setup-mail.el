f80fa2108e733e830a0a199a0362618f4f4c6b95
James Tranovich <jtranovich@gmail.com>
2021-01-16 16:34:22 -0800

    notmuch

--- lisp/setup-mail.el
+++ lisp/setup-mail.el
@@ -1,10 +1,17 @@
 ;;; setup-mail -- Summary
 ;;; Commentary:
-;;; Setup various bits that don't really go anywhere else.
+;;; Setup email configuration.
 ;;; Code:
 (require 'use-package)
 
-(use-package notmuch)
+(use-package notmuch
+  :config
+  (setq notmuch-hello-thousands-separator ","
+        notmuch-hello-sections
+             (list #'notmuch-hello-insert-saved-searches
+	           #'notmuch-hello-insert-search
+	           #'notmuch-hello-insert-recent-searches
+	           #'notmuch-hello-insert-alltags)))
 
 (provide 'setup-mail)
 ;;; setup-mail.el ends here
