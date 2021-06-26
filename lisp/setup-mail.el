a602f8d866d1e3a8fa3bdc22891ebf916c6cb4e1
James Tranovich <jtranovich@gmail.com>
2021-06-26 14:05:34 -0700

    notmuch and maildir specific settings

--- lisp/setup-mail.el
+++ lisp/setup-mail.el
@@ -11,13 +11,23 @@
         notmuch-address-command nil
         notmuch-address-use-company nil ;; do not use notmuch-company for completion
         notmuch-search-oldest-first nil
+        notmuch-fcc-dirs
+        '(("jtranovich@gmail.com" . "\"gmail/[Gmail]/Sent Mail\" -inbox +sent -unread")
+          ("james@tranovich.com" . "\"fastmail/Sent Items\" -inbox +sent -unread"))
         notmuch-hello-sections
              (list #'notmuch-hello-insert-saved-searches
 	           #'notmuch-hello-insert-search
 	           #'notmuch-hello-insert-recent-searches
 	           #'notmuch-hello-insert-alltags)))
 
-(setq message-kill-buffer-on-exit t)
+(setq message-kill-buffer-on-exit t
+      send-mail-function 'sendmail-send-it
+      sendmail-program "msmtp"
+      message-sendmail-envelope-from 'header
+      user-full-name "James Tranovich"
+      mail-specify-envelope-from t
+      mail-host-address "tranovich.com"
+      mail-envelope-from 'header)
 
 ;; custom company backend for contacts
 (require 'khard-contacts)
